a35dcf2d1a90dcae81c1c125ba75c76dfaf3f635
Rohan <rohan.joshi@goodcreator.co>
2023-12-26 11:34:54 +0530

    feat: adding properties to extension

--- lib/src/typeset_ext.dart
+++ lib/src/typeset_ext.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/painting.dart';
 import 'package:typeset/src/typeset.dart';
 
@@ -7,11 +8,41 @@ extension TypeSetExtension on String {
   TypeSet typeset({
     TextStyle? style,
     TextAlign textAlign = TextAlign.start,
+    TextDirection? textDirection,
+    Locale? locale,
+    bool? softWrap,
+    TextOverflow? overflow,
+    TextScaler? textScaler,
+    int? maxLines,
+    String? semanticsLabel,
+    TextWidthBasis? textWidthBasis,
+    TextHeightBehavior? textHeightBehavior,
+    Color? selectionColor,
+    StrutStyle? strutStyle,
+    GestureRecognizer? recognizer,
+    TextStyle? linkStyle,
+    TextStyle? boldStyle,
+    TextStyle? monospaceStyle,
   }) {
     return TypeSet(
       inputText: this,
       style: style,
       textAlign: textAlign,
+      textDirection: textDirection,
+      locale: locale,
+      softWrap: softWrap,
+      overflow: overflow,
+      textScaler: textScaler,
+      maxLines: maxLines,
+      semanticsLabel: semanticsLabel,
+      textWidthBasis: textWidthBasis,
+      textHeightBehavior: textHeightBehavior,
+      selectionColor: selectionColor,
+      strutStyle: strutStyle,
+      recognizer: recognizer,
+      linkStyle: linkStyle,
+      boldStyle: boldStyle,
+      monospaceStyle: monospaceStyle,
     );
   }
 }
