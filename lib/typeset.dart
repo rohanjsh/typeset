e9dff2353befb3c1e3995b6b2c2a4a5d89e1db89
Rohan <rohan.joshi@goodcreator.co>
2024-01-10 12:30:29 +0530

    removing editing controller from exports

--- lib/typeset.dart
+++ lib/typeset.dart
@@ -5,5 +5,4 @@ library typeset;
 export 'src/core/typeset_reserved.dart';
 export 'src/view/typeset.dart';
 export 'src/view/typeset_context_menus.dart';
-export 'src/view/typeset_editing_controller.dart';
 export 'src/view/typeset_ext.dart';
